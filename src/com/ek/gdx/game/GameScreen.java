0bd04564a705a703b9d3859178a6e8a10586e85c
ekarat.st <ekaratdev@gmail.com>
2016-10-06 21:20:31 +0700

    newworld

--- src/com/ek/gdx/game/GameScreen.java
+++ src/com/ek/gdx/game/GameScreen.java
@@ -6,38 +6,58 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter {
 	private PacmanGame pacmanGame;
 	private Texture pacmanImg;
-	private int x;
-    private int y;
+	private Pacman pacman;
+	//private int x;
+    //private int y;
+	
 	 
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
+        
         pacmanImg = new Texture("pacman.png");
-        x = 100;
-        y = 100;
+ 
     }
+    
     @Override
     public void render(float delta) {
     	Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         SpriteBatch batch = pacmanGame.batch;
+        /*
         batch.begin();
         update(delta);
         batch.draw(pacmanImg, x, y);
         batch.end();
+        */
+        batch.begin();
+        //update(delta);
+        Vector2 pos = pacman.getPosition();
+        update(delta);
+        batch.draw(pacmanImg, pos.x, pos.y);
+        batch.end();
     }
+
     private void update(float delta) {
        // x += 5;    
         if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-            x -= 10;
+        	 pacman.move(Pacman.DIRECTION_LEFT);
         }
         if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            x += 10;
+        	 pacman.move(Pacman.DIRECTION_RIGHT);
+        }
+        if(Gdx.input.isKeyPressed(Keys.UP)) {
+        	 pacman.move(Pacman.DIRECTION_UP);
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+        	 pacman.move(Pacman.DIRECTION_DOWN);
         }
     }
+   
     
     
  
