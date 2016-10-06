a59259cad0626f6ffce84e2c7c5b4886db50fea5
ekarat.st <ekaratdev@gmail.com>
2016-10-06 19:50:54 +0700

    show pacmangame

---
+++ src/com/ek/gdx/game/PacmanGame.java
@@ -0,0 +1,34 @@
+package com.ek.gdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class PacmanGame extends Game {
+	public SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new GameScreen(this));
+
+	}
+
+	/*@Override
+    public void render(float delta) {
+        SpriteBatch batch = pacmanGame.batch;
+        batch.begin();
+        batch.draw(pacmanImg, 100, 100);
+        batch.end();
+    }*/
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
+
+
